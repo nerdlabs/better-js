a38b23a673ac3854362ddcce2d5cae6f6f8652c0
ZauberNerd <zaubernerd@zaubernerd.de>
2014-12-07 23:20:21 +0100

    traverse scopechain to see if identifier was already declared

--- index.js
+++ index.js
@@ -7,11 +7,63 @@ var source = fs.readFileSync('messy.js');
 
 var ast = esprima.parse(source);
 
+var scopes = [];
+
+function isVariable(node) {
+  return node.type === 'VariableDeclaration';
+}
+
+function isFunction(node) {
+  return node.type === 'FunctionDeclaration';
+}
+
+function getVariableName(node) {
+  return node.id.name;
+}
+
+function getVariableNames(node) {
+  return node.declarations.map(getVariableName);
+}
+
+function merge(array1, array2) {
+  return array1.concat(array2);
+}
+
+function getScopeIdentifiers(body) {
+  var vars  = body.filter(isVariable).map(getVariableNames).reduce(merge, []);
+  var funcs = body.filter(isFunction).map(getVariableName);
+  return vars.concat(funcs);
+}
+
+function getArguments(node) {
+  return node.params.map(function (param) { return param.name; });
+}
+
+function isDeclared(name) {
+  return scopes.some(function (scope) {
+    return scope.indexOf(name) !== -1;
+  });
+}
+
 estraverse.traverse(ast, {
   enter: function(node) {
-    if (node.type === 'CallExpression' && node.callee.name === 'isNaN') {
-      node = replaceIsNaN(node);
+    var scope = [];
+    if (Array.isArray(node.body)) {
+      scope.push.apply(scope, getScopeIdentifiers(node.body));
     }
+    if (isFunction(node)) {
+      scope.push.apply(scope, getArguments(node));
+    }
+    scopes.push(scope);
+
+    if (node.type === 'CallExpression' && node.callee.name === 'isNaN') {
+      if (!isDeclared('isNaN')) {
+        node = replaceIsNaN(node);
+      }
+    }
+  },
+  leave: function (node, parent) {
+    scopes.pop();
   }
 });
 
