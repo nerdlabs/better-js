6892b6f99f97f51ee4e103c5a4f5ace6d1cea9ec
ZauberNerd <zaubernerd@zaubernerd.de>
2014-12-07 21:04:25 +0100

    print generated code

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
 var esprima = require('esprima');
+var escodegen = require('escodegen');
 
 var source = fs.readFileSync('messy.js');
 
 
 
-console.log(JSON.stringify(esprima.parse(source), null, 4));
+// console.log(JSON.stringify(esprima.parse(source), null, 4));
 
+console.log(escodegen.generate(esprima.parse(source)));
