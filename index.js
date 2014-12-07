a905e2832586272e65ed384ae2eef401da3d5915
Robert Kowalski <rok@kowalski.gd>
2014-12-07 21:20:13 +0100

    working isNaN POC

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-var fs = require('fs');
-var esprima = require('esprima');
+var fs = require('fs'),
+    esprima = require('esprima'),
+    estraverse = require('estraverse'),
+    escodegen = require('escodegen');
 
 var source = fs.readFileSync('messy.js');
 
+var ast = esprima.parse(source);
+
+estraverse.traverse(ast, {
+  enter: function(node) {
+    if (node.type === 'CallExpression') {
+      node = replaceIsNaN(node);
+    }
+  }
+});
+
+var res = escodegen.generate(ast);
 
 
-console.log(JSON.stringify(esprima.parse(source), null, 4));
+function replaceIsNaN (node) {
+  node.callee = {
+    type: 'MemberExpression',
+    computed: false,
+    object: { type: 'Identifier', name: 'Number' },
+    property: { type: 'Identifier', name: 'isNaN' }
+  };
+  return node;
+}
+
+console.log(res);
 
